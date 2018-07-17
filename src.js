30af92c6465febe10564eee9531eb6b39b11947b
Isaac Dozier <isaactdozier@gmail.com>
2018-07-17 06:51:51 +0000

    v0.0.3-beta: improve performance

--- src.js
+++ src.js
@@ -6,24 +6,25 @@ var prs = []
 
 function load(){
 	prs = []
+	document.getElementById('output').innerHTML = ''
 	document.getElementById('title').innerHTML = title
 }
 
 
 function generate_prime_numbers(){
 	
-	prs = []
-	document.getElementById('output').innerHTML = ''
+	load()
+	
 	
 	var p = prs.length
-	
 	for(var i = limit('a'); i < limit('b'); i++){
-		var s = 0
-		
+		var t = i.toString().split('')
+		if(exclude(t.indexOf(t.length-1)))
+			break
 		for(var d = 2; d < i; d++){
 			if((i/d)*1000 === Math.ceil(i/d)*1000)
-				s=1
-			if(d === i-1 && s === 0)
+				break
+			if(d === i-1)
 				prs.push(i)
 		}
 	}
@@ -32,6 +33,13 @@ function generate_prime_numbers(){
 		prs.unshift(2)
 		
 	document.getElementById('output').innerHTML = prs.join(', ')
+}
+
+function exclude(n){
+	var dgt = [2,4,5,6,8]
+	for(var i = 0; i < dgt.length; i++){
+		return Number(n) === dgt[i]
+	}
 }
 
 function limit(r){
